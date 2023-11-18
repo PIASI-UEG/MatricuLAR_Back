a6fe2a7ef668472a6095a001700c6a9fae642daa
Adelane Junior <adelane.junior@aluno.ueg.br>
2023-11-18 17:31:23 -0300

    Initial commit

---
+++ src/main/java/br/ueg/piasi/MatricuLAR/MatricuLarApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.piasi.MatricuLAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatricuLarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatricuLarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/piasi/MatricuLAR/MatricuLarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.piasi.MatricuLAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatricuLarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
