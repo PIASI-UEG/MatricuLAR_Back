d4d33798d4e39a89313f914e4999b3715ba87617
lucashspiess <lucashs725@gmail.com>
2023-11-26 19:26:54 -0300

    UPDATE add das anotacoes @EntityScan e @EnableWebSecurity para funcionamento da arquitetura

--- src/main/java/br/ueg/piasi/MatricuLAR/MatricuLarApplication.java
+++ src/main/java/br/ueg/piasi/MatricuLAR/MatricuLarApplication.java
@@ -2,8 +2,20 @@ package br.ueg.piasi.MatricuLAR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"br.ueg.piasi.MatricuLAR.*",
+		"br.ueg.prog.webi.*" //Para funcionamento da Arquitetura
+})
+@EntityScan(basePackageClasses = {Jsr310JpaConverters.class},
+		basePackages = {
+				"br.ueg.piasi.MatricuLAR.*",
+				"br.ueg.prog.webi.api.*" //Para funcionamento da Arquitetura
+		})
+@EnableWebSecurity
 public class MatricuLarApplication {
 
 	public static void main(String[] args) {
