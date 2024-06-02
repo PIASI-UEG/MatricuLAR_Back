8f7eb72e821ddb704f07792cd57d4e53259df0c5
Adelane Junior <adelane.junior13@gmail.com>
2024-06-02 17:42:55 -0300

    adcionando para inclusão de matriculas sem token

--- src/main/java/br/ueg/piasi/MatricuLAR/configuration/SecurityConfig.java
+++ src/main/java/br/ueg/piasi/MatricuLAR/configuration/SecurityConfig.java
@@ -61,7 +61,8 @@ public class SecurityConfig extends ApiSecurityConfig {
                         "/webjars/**",
                         "/swagger-ui.html",
                         "/api/v1/usuario/redefinir-senha",
-                        "/api/v1/matricula/inclusao-com-docs"));
+                        "/api/v1/matricula/inclusao-com-docs",
+                        "/api/v1/matricula/inclusao-com-docs/*"));
         freeAccessPaternsList.addAll(getCustomFreeAccessPaterns());
         String[] freeAccessPaterns = freeAccessPaternsList.toArray(new String[0]);
         http
